e69ceff49704745db222151fcd8a52dea6a01ec2
johnff9 <157079795+johnff9@users.noreply.github.com>
2024-10-31 20:28:54 -0700

    Spotify Login Issue

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 const client_id = 'b9562b5e82584ab795597da970805681';
 const client_secret = 'df6276ebac6b417ea947edf834747dc6';
 let access_token = 'your-access-token'; // Ideally obtained through OAuth 2.0 and refreshed as needed
-
-var authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: {
-      'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
-    },
-    form: {
-      grant_type: 'client_credentials'
-    },
-    json: true
-  };
-
+//Having issues of getting a permanent access token to display on my currently playing information rather
+//than a user logging into the webpage for their spotify information
 async function fetchCurrentTrack() {
   const response = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
     headers: {
-        
+      'Authorization': `Bearer ${access_token}`
     }
   });
 
